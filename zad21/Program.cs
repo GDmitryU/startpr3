aeec6eb4f1f1317634906946e91b90ea15dd9ade
DmitryG <dmitrygribchenko@mail.ru>
2022-08-31 02:01:03 +0300

    zad23

---
+++ zad21/Program.cs
@@ -0,0 +1,16 @@
+﻿double x1, x2, y1, y2, z1, z2;
+Console.WriteLine("Введите первую координату первой точки");
+x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату второй точки");
+x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату первой точки");
+y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату второй точки");
+y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третью координату первой точки");
+z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третьюю координату второй точки");
+z2 = double.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+Console.WriteLine(result);
---
+++ zad23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int next =1;
+while (next <= n){
+   
+    int cube = next*next*next;
+    next++;
+    Console.Write($"{cube}, ");
+}
+
